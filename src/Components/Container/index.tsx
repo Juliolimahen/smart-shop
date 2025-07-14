15b333eee050d6fd7c3f72fb11408953b5c580d5
juliolimahen <juliolima_henrique@outlook.com>
2025-07-14 03:41:29 -0300

    criando e estlizando container principal

--- src/Components/Container/index.tsx
+++ src/Components/Container/index.tsx
@@ -1,33 +1,22 @@
-import styled from "styled-components";
-import Titulo from "../Titulo";
-import Home from "../../pages/Home";
-import Favorites from "../../pages/Favorites";
-import Footer from "../Footer";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../../pages/Home';
+import Favorites from '../../pages/Favorites';
+import Footer from '../Footer';
+import { Container } from './styles';
 
-const Container = styled.div`
-  background-color: ${({ theme }) => theme.body};
-  min-height: 90vh;
-  padding: 0px 15vw;  
-`
-const Conteudo = styled.section`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  color: ${({ theme }) => theme.text};
-  @media (max-width: 800px) {
-      flex-direction: column;
-  }
-`
-export default () => {
+const MainContent: React.FC = () => {
   return (
     <>
-      <Container><Titulo>Olá Fulano!</Titulo>
-        <Conteudo>
-          <Home />
-          <Favorites />
-        </Conteudo>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favoritos" element={<Favorites />} />
+        </Routes>
       </Container>
       <Footer />
     </>
   );
 };
+
+export default MainContent;
---
+++ src/Components/Container/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: ${({ theme }) => theme.body};
+  min-height: 90vh;
+  padding: 0px 15vw;
+`;
