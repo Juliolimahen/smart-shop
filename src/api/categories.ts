d85cb17f202fccc349e86aece288c7f4b53e6e16
juliolimahen <juliolima_henrique@outlook.com>
2025-07-14 03:37:37 -0300

    criar service para simular api endpoint categorias

--- src/api/categories.ts
+++ src/api/categories.ts
@@ -3,9 +3,9 @@ import type { Category } from '../types';
 
 export const categories: Category[] = categoriesData;
 
-export const getCategoryName = (id: number): string =>
-  categories.find(cat => cat.id === id)?.nome || 'Categoria vazia';
-
 export const getAllCategories = (): Category[] => {
   return categories;
 };
+
+export const getCategoryName = (id: number): string =>
+  categories.find(cat => cat.id === id)?.nome || 'Categoria não encontrada';
